0df92cac81f37194e71a782007e46c78b1c0c1c5
Josh Winters <jwinters@pivotal.io>
2019-12-11 11:03:58 -0500

    Add support for client_credentials grant type
    Signed-off-by: Rui Yang <ryang@pivotal.io>

--- server/oauth2.go
+++ server/oauth2.go
@@ -121,6 +121,7 @@ const (
 const (
 	grantTypeAuthorizationCode = "authorization_code"
 	grantTypeRefreshToken      = "refresh_token"
+	grantTypeClientCredentials = "client_credentials"
 )
 
 const (
